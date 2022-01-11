fb844d8169bb7dd83d99524df45d0425cd1ac09e
Hereloss <Hereloss@googlemail.com>
2022-01-11 14:59:03 +0000

    Part 13 done

--- bundle.js
+++ bundle.js
@@ -25,9 +25,44 @@
     }
   });
 
+  // notesView.js
+  var require_notesView = __commonJS({
+    "notesView.js"(exports, module) {
+      var NotesModel2 = require_notesModel();
+      var NotesView2 = class {
+        constructor(model) {
+          this.model = model;
+          this.mainContainerEl = document.querySelector("#main-container");
+          document.querySelector("#add-note-btn").addEventListener("click", () => {
+            const newNote = document.querySelector("#add-note-input").value;
+            this.addNewNote(newNote);
+          });
+        }
+        addNewNote(newNote) {
+          this.model.addNotes(newNote);
+          this.displayNotes();
+        }
+        displayNotes() {
+          const mynotes = this.model.getNotes();
+          console.log(this.model.getNotes());
+          mynotes.forEach((element) => {
+            let div = document.createElement("div");
+            div.innerText = element;
+            div.className = "note";
+            this.mainContainerEl.append(div);
+          });
+        }
+      };
+      module.exports = NotesView2;
+    }
+  });
+
   // index.js
   var NotesModel = require_notesModel();
+  var NotesView = require_notesView();
   console.log("The notes app is running");
   var notes = new NotesModel();
-  console.log(notes.getNotes());
+  var viewer = new NotesView(notes);
+  notes.addNotes("This is an example note");
+  viewer.displayNotes();
 })();
--- notesView.js
+++ notesView.js
@@ -1,13 +1,25 @@
 const NotesModel = require('./notesModel')
 
 class NotesView {
-  constructor(notes = new NotesModel()) {
+  constructor(model) {
+    this.model = model;
     this.mainContainerEl = document.querySelector('#main-container');
-    this.notes = notes
+
+    document.querySelector('#add-note-btn').addEventListener('click', () => {
+      const newNote = document.querySelector('#add-note-input').value;
+      this.addNewNote(newNote);
+    });
   }
 
+  addNewNote(newNote) {
+    this.model.addNotes(newNote);
+    this.displayNotes();
+  }
+
+
   displayNotes() {
-    const mynotes = this.notes.getNotes()
+    const mynotes = this.model.getNotes()
+    console.log(this.model.getNotes())
     mynotes.forEach( element=> {
       let div = document.createElement("div");
       div.innerText = element
